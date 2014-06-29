19f80053aea39bb6a40f34bdd444412101b565bd
BrandiATMuhkuh <juergen@tagaprice.org>
2014-06-29 00:12:24 -0400

    add produc list dump file

---
+++ productPages.js
@@ -0,0 +1,7 @@
+/*
+//http://shop.countdown.co.nz/Shop/Browse/liquor-beer-cider?page=1
+
+$(".details-container").each(function(){
+console.log($(this).children().first().children().first().attr("href"))
+})
+*/
